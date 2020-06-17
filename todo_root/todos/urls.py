4bd344fbd85afc5fbfe39d2209dbabad79d36c4f
Julian West <west221b@gmail.com>
2020-06-17 17:36:05 -0400

    Initial, Release 0

---
+++ todo_root/todos/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'todos'
+urlpatterns = [
+    # path('', views.IndexView.as_view(), name='index'),
+    # path('new/', views.FormView.as_view(), name='new'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+]
