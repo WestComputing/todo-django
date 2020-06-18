27f420b66bd2d44f6685de974c2d27d593009635
Julian West <west221b@gmail.com>
2020-06-17 23:12:28 -0400

    Add index view

--- todo_root/todos/views.py
+++ todo_root/todos/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Todo
 
 
 class IndexView(generic.ListView):
-    pass
+    template_name = 'todos/index.html'
+    context_object_name = 'todo_list'
+
+    def get_queryset(self):
+        return Todo.objects.all()
 
 
 class DetailView(generic.DetailView):
