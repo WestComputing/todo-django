d71894f17b41fd377ca68389741f7dde03cdd9de
Julian West <west221b@gmail.com>
2020-06-23 09:13:37 -0400

    Add authentication: login, logout, signup

--- todo_root/todo/settings.py
+++ todo_root/todo/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'todos.apps.TodosConfig'
+    'todos.apps.TodosConfig',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
