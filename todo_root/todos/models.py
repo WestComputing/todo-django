d6a8671a3483d17d0eb1f82f5f36002229b4ab22
Julian West <west221b@gmail.com>
2020-06-24 01:04:19 -0400

    Apply authentication; add authorization

--- todo_root/todos/models.py
+++ todo_root/todos/models.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
@@ -10,6 +11,7 @@ class Todo(models.Model):
     completed = models.BooleanField(default=False)
     created = models.DateField(default=timezone.now)
     due = models.DateField(null=True, default=timezone.now() + timedelta(days=7))
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def due_today(self):
         return bool(self.due == timezone.localdate())
--- todo_root/todos/views.py
+++ todo_root/todos/views.py
@@ -1,29 +1,41 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, ModelFormMixin
 
 from .models import Todo
 
 
-class IndexView(ListView):
+class UserMatchesTodo(UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user == self.get_object().user
+
+
+class FormStampUser(ModelFormMixin):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class IndexView(LoginRequiredMixin, ListView):
     template_name = 'todos/index.html'
     context_object_name = 'todo_list'
 
     def get_queryset(self):
-        return Todo.objects.all().order_by('due')
+        return Todo.objects.filter(user_id=self.request.user.id).order_by('due')
 
 
-class ItemView(DetailView):
+class ItemView(LoginRequiredMixin, UserMatchesTodo, DetailView):
     model = Todo
 
 
-class NewFormView(CreateView):
+class NewFormView(LoginRequiredMixin, CreateView, FormStampUser):
     model = Todo
     fields = ['title', 'description', 'completed', 'due']
     success_url = reverse_lazy('todos:index')
 
 
-class EditFormView(UpdateView):
+class EditFormView(LoginRequiredMixin, UserMatchesTodo, UpdateView, FormStampUser):
     model = Todo
     fields = ['title', 'description', 'completed', 'created', 'due']
     template_name_suffix = '_edit_form'
@@ -32,6 +44,6 @@ class EditFormView(UpdateView):
         return reverse('todos:detail', kwargs={'pk': self.object.pk})
 
 
-class DeleteFormView(DeleteView):
+class DeleteFormView(LoginRequiredMixin, UserMatchesTodo, DeleteView):
     model = Todo
     success_url = reverse_lazy('todos:index')
